c330b6b10bcbe0f2d404f17beeef2984af873cdb
Edwin Senjaya <edwin.senjaya@digdayatech.com>
2023-06-09 17:22:05 +0700

    project details

---
+++ pages/project/[name]/index.js
@@ -0,0 +1,27 @@
+import dynamic from "next/dynamic";
+import Head from "next/head";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+const ProjectBanner = dynamic(() => import("@/components/ProjectBanner"), {
+  ssr: false,
+});
+
+const ProjectDetails = dynamic(() => import("@/components/ProjectDetails"), {
+  ssr: false,
+});
+
+export default function Page() {
+  return (
+    <>
+      <Head>
+        <title>Arsir Architect</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Header></Header>
+      <ProjectBanner></ProjectBanner>
+      <ProjectDetails></ProjectDetails>
+      <Footer></Footer>
+    </>
+  );
+}
