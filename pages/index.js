3368e10a2f7faef6995e080733e3db8c893e3aea
Edwin Senjaya <edwin.senjaya@digdayatech.com>
2023-06-11 18:00:18 +0700

    fix hero banner

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,11 @@ import dynamic from "next/dynamic";
 
 import Head from "next/head";
 import Header from "@/components/Header";
-import HeroBanner from "@/components/HeroBanner";
 import CoreValue from "@/components/CoreValue";
 import Footer from "@/components/Footer";
+const HeroBanner = dynamic(() => import("@/components/HeroBanner"), {
+  ssr: false,
+});
 const Thumbnail = dynamic(() => import("@/components/Thumbnail"), {
   ssr: false,
 });
