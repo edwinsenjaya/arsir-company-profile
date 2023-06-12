d3fd2326046200501792b730a41d603d700e8a89
Edwin Senjaya <edwin.senjaya@digdayatech.com>
2023-06-12 22:59:19 +0700

    what we do section and footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,44 @@
 import styles from "@/styles/footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
-  return <section className={styles["footer-container"]}>Footer</section>;
+  return (
+    <section className={styles["footer-container"]}>
+      <section className={styles["social-media"]}>
+        <Link href="mailto:arsirarchitect@gmail.com" target="_blank">
+          <Image
+            src="/footer/email.png"
+            width={55}
+            height={55}
+            alt="email.png"
+          />
+        </Link>
+        <Link href="https://www.instagram.com/arsirarchitect/" target="_blank">
+          <Image
+            src="/footer/instagram.png"
+            width={55}
+            height={55}
+            alt="instagram.png"
+          />
+        </Link>
+        <Link href="https://wa.me/6281389007566" target="_blank">
+          <Image
+            src="/footer/whatsapp.png"
+            width={55}
+            height={55}
+            alt="whatsapp.png"
+          />
+        </Link>
+      </section>
+      <section>
+        <Image
+          src="/images/arsir-architect-white-1.png"
+          width={100}
+          height={100}
+          alt="email.png"
+        />
+      </section>
+    </section>
+  );
 }
--- components/WhatWeDo.js
+++ components/WhatWeDo.js
@@ -5,14 +5,18 @@ import { useState } from "react";
 export default function WhatWeDo() {
   const [whatWeDo, setWhatWeDo] = useState({
     research:
-      "Providing consultation for client's needs and wants, collecting project's surrounding environment data.",
+      "Providing consultation for client's needs and wants, collecting project's surrounding environment data",
     schematic:
-      "Creating early design concepts and projecting ideas in a 3D form.",
+      "Creating early design concepts and projecting ideas in a 3D form",
     drawing:
-      "Translating the design into technical drawings for construction purposes.",
+      "Translating the design into technical drawings for construction purposes",
     supervise:
-      "Monitoring the construction progress and solving problems on site.",
+      "Monitoring the construction progress and solving problems on site",
   });
+
+  function capitalize(word) {
+    return word[0].toUpperCase() + word.substring(1);
+  }
 
   const whatWeDoContent = [];
   for (let key in whatWeDo) {
@@ -25,14 +29,18 @@ export default function WhatWeDo() {
           alt={`/what-we-do/${key}.png`}
           className={styles.image}
         />
+        <h3 className={styles.title}>{capitalize(key)}</h3>
         <p className={styles.description}>{whatWeDo[key]}</p>
       </section>
     );
   }
 
   return (
-    <section className={styles["what-we-do-container"]}>
-      {whatWeDoContent}
-    </section>
+    <>
+      <h3 className={styles["what-we-do-title"]}>What We Do</h3>
+      <section className={styles["what-we-do-container"]}>
+        {whatWeDoContent}
+      </section>
+    </>
   );
 }
