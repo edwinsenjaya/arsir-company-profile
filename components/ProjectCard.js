36f620ff8e6f4a6e9c997f3702a0e5e6962f3f8a
Edwin Senjaya <edwin.senjaya@digdayatech.com>
2023-06-19 23:19:14 +0700

    rework thumbnail, no random, add 2 new projects

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,9 +1,119 @@
 import styles from "@/styles/project-card.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function ProjectCard() {
-  const projects = JSON.parse(localStorage.getItem("projects"));
+  const [projects, setProjects] = useState([
+    {
+      name: "OJ House",
+      type: "Residential",
+      year: "2023",
+      location: "Daan Mogot, West Jakarta",
+      area: "185 m",
+      scope: "Exterior",
+      numberOfImages: 4,
+    },
+    {
+      name: "JH House",
+      type: "Residential",
+      year: "2023",
+      location: "Tanjung Duren, West Jakarta",
+      area: "275 m",
+      scope: "Exterior",
+      numberOfImages: 4,
+    },
+    {
+      name: "DJ House",
+      type: "Residential",
+      year: "2023",
+      location: "Penyaringan, Bali",
+      area: "405 m",
+      scope: "Exterior",
+      numberOfImages: 10,
+    },
+    {
+      name: "SA House",
+      type: "Residential",
+      year: "2023",
+      location: "Alam Sutera, South Tangerang",
+      area: "410 m",
+      scope: "Exterior",
+      numberOfImages: 4,
+    },
+    {
+      name: "JN Resort",
+      type: "Resort",
+      year: "2023",
+      location: "Nusa Penida",
+      area: "800 m",
+      scope: "Exterior & Interior",
+      numberOfImages: 4,
+    },
+    {
+      name: "KS House",
+      type: "Residential",
+      year: "2023",
+      location: "Kavling DKI, West Jakarta",
+      area: "175 m",
+      scope: "Exterior",
+      numberOfImages: 4,
+    },
+    {
+      name: "SV House",
+      type: "Residential",
+      year: "2023",
+      location: "BSD, South Tangerang",
+      area: "420 m",
+      scope: "Exterior",
+      numberOfImages: 5,
+    },
+    {
+      name: "CC House",
+      type: "Residential",
+      year: "2022",
+      location: "Taman Kopo, Bandung",
+      area: "130 m",
+      scope: "Design & Build",
+      numberOfImages: 4,
+    },
+    {
+      name: "GB Apartment",
+      type: "Residential",
+      year: "2019",
+      location: "Bintaro, South Tangerang",
+      area: "21.5 ~ 45.6 m",
+      scope: "Interior",
+      numberOfImages: 4,
+    },
+    {
+      name: "TC House",
+      type: "Residential",
+      year: "2021",
+      location: "Citraland, Makassar",
+      area: "240 m",
+      scope: "Interior",
+      numberOfImages: 8,
+    },
+    {
+      name: "AW House",
+      type: "Residential",
+      year: "2020",
+      location: "Westhoff, Bandung",
+      area: "570 m",
+      scope: "Facade",
+      numberOfImages: 8,
+    },
+    {
+      name: "YB House",
+      type: "Residential",
+      year: "2020",
+      location: "Bone, South Sulawesi",
+      area: "42 m",
+      scope: "Interior",
+      numberOfImages: 5,
+    },
+  ]);
 
   const cards = [];
 
