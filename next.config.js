e2557a8d0bb156ceb04028659bd29162d1ca6af8
Edwin Senjaya <edwin.senjaya@digdayatech.com>
2023-07-26 15:12:39 +0700

    fix NJ_House image

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const webpack = require("webpack");
+
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
