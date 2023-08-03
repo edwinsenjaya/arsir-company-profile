256cb2005dc90c2f928322582c2c1a94b3cca08b
Edwin Senjaya <edwin.senjaya@digdayatech.com>
2023-08-03 14:51:29 +0700

    fix error

--- components/ProjectDetails.js
+++ components/ProjectDetails.js
@@ -2,7 +2,6 @@ import styles from "@/styles/project-details.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { DJ_House } from "@/public/blur/blur";
 
 export default function ProjectDetails() {
   const projectsData = JSON.parse(localStorage.getItem("projectsData"));
