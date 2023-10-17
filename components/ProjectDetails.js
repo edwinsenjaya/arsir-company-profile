3f444b0458fd87fb2cbe28165cac3374105ccd4b
Edwin Senjaya <edwin.senjaya@digdayatech.com>
2023-10-17 15:14:07 +0700

    fix project detail

--- components/ProjectDetails.js
+++ components/ProjectDetails.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 
 export default function ProjectDetails() {
+  const previousProject = localStorage.getItem("projectDetailName");
   const router = useRouter();
   let projectName = router.query.name;
 
